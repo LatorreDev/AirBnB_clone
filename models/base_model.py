be334686b393cbf2337e632a0dd3c4980aa7606a
David Latorre <david@latorredev.com>
2019-06-29 17:40:15 -0500

    [Feature] add instance and class

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+from uuid import uuid4
+import datetime
+
+class BaseModel:
+    id = ""
+    create_at = 0
+    update_at = 0
+    def __init__(self, id, created_at, updated_at):
+        my_time = datetime.datetime.now()
+        self.id = str(uuid4())
+        self.create_at = my_time
+        self.update_at = my_time
+    def __str__(self):
+        print(type(self))
+
