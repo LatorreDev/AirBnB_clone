809825ffaf71c408dacfb583f50bdca94418a406
David Latorre <david@latorredev.com>
2019-06-30 18:05:59 -0500

    [Update] Add attributtes for this filestorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,5 +2,18 @@
 
 import json
 
-class Filestorage:
-    pass
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        pass
+
+    def save(self):
+        pass
+
+    def reload(self):
+        pass
