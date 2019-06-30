8a20db9c51894bb8e0cfa3606616f8ac08ea174f
David Latorre <david@latorredev.com>
2019-06-29 20:30:06 -0500

    [Feature] add str and save functions

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,17 @@ class BaseModel:
     id = ""
     create_at = 0
     update_at = 0
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         my_time = datetime.datetime.now()
         self.id = str(uuid4())
         self.create_at = my_time
         self.update_at = my_time
     def __str__(self):
-        print(type(self))
-
+        my_name = "[<" + self.__class__.__name__ + ">]"
+        my_id = str("(<" + self.id + ">)")
+        my_dict = str(self.__dict__)
+        return ( my_name + " " + my_id + " " + my_dict)
+    def save(self):
+        self.update_at = datetime.datetime.now()
+    def to_dict(self):
+        print (self.__dict__)
