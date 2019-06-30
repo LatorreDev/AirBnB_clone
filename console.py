d50823065da9c1b4cef88a6d5e30f361ada80cef
David Latorre <david@latorredev.com>
2019-06-29 20:55:12 -0500

    [Fix] deleted help method, reserved method

--- console.py
+++ console.py
@@ -6,13 +6,11 @@ class HBNBCommand(cmd.Cmd):
     """Interpeter"""
     prompt = "(hbnb)"
 
-    def do_help(self, args):
-        'Help of commands'
     def do_quit(self, args):
-        'exit interpeter'
+        """exit interpeter"""
         return True
     def do_EOF(self, args):
-        'exit interpeter'
+        """exit interpeter"""
         return True
 if __name__ == '__main__':
     MY_INTERPETER = HBNBCommand()
